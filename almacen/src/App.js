398e2bfc0aab65529d594bca6dcec8da318a00c7
Eme112 <eme_pedraza@hotmail.com>
2022-07-16 01:28:09 -0500

    Reorganized and added NavBar

--- almacen/src/App.js
+++ almacen/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from './components/NavBar';
-import Home from './components/Home'
-import Login from './components/Login'
-import Search from './components/Search'
-import MaterialInfo from './components/MaterialInfo'
-import MaterialReservation from './components/MaterialReservation'
-import AddMaterial from './components/AddMaterial'
-import RequestMaterial from './components/RequestMaterial';
+import Home from './components/home/Home'
+import Login from './components/login/Login'
+import Search from './components/search/Search'
+import MaterialInfo from './components/material-info/MaterialInfo'
+import MaterialReservation from './components/material-reservation/MaterialReservation'
+import AddMaterial from './components/add-material/AddMaterial'
+import RequestMaterial from './components/request-material/RequestMaterial';
 
 
 function App() {
@@ -16,12 +18,12 @@ function App() {
     <NavBar />
       <Routes>
         <Route path="/" element={<Home />}   />
-        <Route path="/" element={<Login />}   />
-        <Route path="/" element={<Search />}   />
-        <Route path="/" element={<MaterialInfo />}   />
-        <Route path="/" element={<MaterialReservation />}   />
-        <Route path="/" element={<AddMaterial />}   />
-        <Route path="/" element={<RequestMaterial />}   />
+        <Route path="/login" element={<Login />}   />
+        <Route path="/search" element={<Search />}   />
+        <Route path="/material-info" element={<MaterialInfo />}   />
+        <Route path="/material-reservation" element={<MaterialReservation />}   />
+        <Route path="/add-material" element={<AddMaterial />}   />
+        <Route path="/request-material" element={<RequestMaterial />}   />
       </Routes>
     </BrowserRouter>
   );
