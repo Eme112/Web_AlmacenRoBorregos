6bb7bc885af2b54e5a7bf797865a28436e6b4195
Eme112 <eme_pedraza@hotmail.com>
2022-07-21 15:56:13 -0700

    Modified request material

--- almacen/src/components/request-material/RequestMaterial.js
+++ almacen/src/components/request-material/RequestMaterial.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import RequestForm from './RequestForm'
 
 function RequestMaterial() {
   return (
     <div class="inside_page">
       <h2 class="page_heading">Solicitar compra</h2>
+      <RequestForm />
     </div>
   )
 }
