e0afcf9493d32c60b92b6af44302d25a081f1a76
Eme112 <eme_pedraza@hotmail.com>
2022-07-21 16:49:36 -0700

    Advancements on front-end

---
+++ almacen/src/components/add-material/AddForm.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Quantity from '../Quantity'
+
+function AddForm() {
+  return (
+    <div>
+      <br/>
+      <p>
+        Nombre del material
+        <input class="form-control" id="NombreMaterial"/>
+      </p>
+      <p>
+        Cantidad a solicitar 
+        <Quantity />
+      </p>
+      <p>
+        Motivo
+        <input class="form-control" id="Motivo" placeholder="Escribe aquí el motivo de la solicitud de compra"/>
+      </p>
+      <p>
+        Link de compra 1
+        <input class="form-control" id="Link1"/>
+      </p>
+      <p>
+        Link de compra 2
+        <input class="form-control" id="Link2"/>
+      </p>
+    </div>
+  )
+}
+
+export default AddForm
--- almacen/src/components/material-reservation/MaterialReservation.js
+++ almacen/src/components/material-reservation/MaterialReservation.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ReservationForm from './ReservationForm'
 
 function MaterialReservation() {
   return (
     <div class="inside_page">
       <h2 class="page_heading">Reserva de material</h2>
+      
+      <ReservationForm />
     </div>
   )
 }
