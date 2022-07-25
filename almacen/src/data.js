0e129e810888ac381f6579beb3ece9f132ec79e3
montsecc00 <montsecc@live.com.mx>
2022-07-25 17:43:45 -0500

    v1.1 missing images

--- almacen/src/data.js
+++ almacen/src/data.js
@@ -11,14 +11,14 @@
   export const materialesLista = [
     {
       id:1,
-      img:"https://www.mouser.mx/images/marketingid/2016/img/QFP-64_Microchip.png?v=031122.0504"
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
     },
     {
       id:2,
-      img:"https://arduino.cl/wp-content/uploads/2019/01/Arduino-UNO-2.png"
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
     },
     {
       id:3,
-      img:"https://www.dlf.pt/dfpng/middlepng/563-5630382_raspberry-pi-3-model-b-2018-raspberry-pi.png"
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
     },
   ]; 
