b1ecc79fd34da24a821dd403c86bf1dba06e915b
Daniel Pardo <pardaniel17@gmail.com>
2022-07-25 14:47:13 -0500

    login and register pages

--- almacen/src/index.js
+++ almacen/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from './pages/Login';
-import MaterialInfo from './pages/MateialInfo';
+import Register from './pages/Register';
 import Reservar from './pages/Reservar';
 
 ReactDOM.render(
@@ -12,7 +12,7 @@ ReactDOM.render(
     <Routes>
       <Route path='/' element={<App/>}/>
       <Route path='/login' element={<Login/>}/>
-      <Route path='/register' element={<MaterialInfo/>}/>
+      <Route path='/register' element={<Register/>}/>
       <Route path='/reservar' element={<Reservar/>}/>
     </Routes>
   </Router>,\
