60ccda5fbde620a644bd47542492cc4489f4e66e
Daniel Pardo <pardaniel17@gmail.com>
2022-07-25 12:37:36 -0500

    avance front

--- almacen/src/components/add-material/AddMaterial.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AddMaterial() {
-  return (
-    <div>AddMaterial</div>
-  )
-}
-
-export default AddMaterial
--- almacen/src/index.js
+++ almacen/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Login from './pages/Login';
+import MaterialInfo from './pages/MateialInfo';
+import Reservar from './pages/Reservar';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<MaterialInfo/>}/>
+      <Route path='/reservar' element={<Reservar/>}/>
+    </Routes>
+  </Router>,\
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
