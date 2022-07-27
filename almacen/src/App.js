0a8d96cb1821016fa41ac345df7d6b3c13f1a063
Eme112 <eme_pedraza@hotmail.com>
2022-07-27 02:41:19 -0500

    Revert "tried to place everything inside src"
    This reverts commit ca49ec3a4f1bc6a8c62a2f7797d4e55d4c8485cf.

--- almacen/src/App.js
+++ almacen/src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from '../components/NavBar';
-import Home from '../components/home/Home'
-import Login from '../components/login/Login'
-import Search from '../components/search/Search'
-import MaterialInfo from '../components/material-info/MaterialInfo'
-import MaterialReservation from '../components/material-reservation/MaterialReservation'
-import AddMaterial from '../components/add-material/AddMaterial'
-import RequestMaterial from '../components/request-material/RequestMaterial';
+import NavBar from './components/NavBar';
+import Home from './components/home/Home'
+import Login from './components/login/Login'
+import Search from './components/search/Search'
+import MaterialInfo from './components/material-info/MaterialInfo'
+import MaterialReservation from './components/material-reservation/MaterialReservation'
+import AddMaterial from './components/add-material/AddMaterial'
+import RequestMaterial from './components/request-material/RequestMaterial';
 
 
 function App() {
